0ec687d8c31bf17fa20adb63d5d9af502aedb948
Sionna Poleszuk <sionna.pol@gmail.com>
2021-12-30 09:54:06 +0100

    heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* global require, process */
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('dist/db/app.json');
@@ -5,9 +6,8 @@ const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true
 });
-const port = process.env.PORT || 3131;
 
+const port = process.env.PORT || 3131;
 server.use(middlewares);
 server.use(router);
-
 server.listen(port);
